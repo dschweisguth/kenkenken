fe51fa3cf046f903c4730a9972f36e2dd13888d7
Dave Schweisguth <dave@schweisguth.org>
2022-09-20 04:53:04 -0700

    Refactor Sum

--- lib/box/sum.rb
+++ lib/box/sum.rb
@@ -2,12 +2,12 @@ require_relative 'base'
 
 class Box::Sum < Box::Base
   def solve
-    solved_cells, unsolved_cells = @cells.partition { |_, cell| cell.solution }
+    solved_cells, unsolved_cells = @cells.values.partition &:solution
     if unsolved_cells.length != 1
       return false
     end
-    remainder = @result - solved_cells.map { |_, cell| cell.possibilities.first }.sum
-    unsolved_cells.first[1].solution = remainder
+    remainder = @result - solved_cells.map { |cell| cell.solution }.sum
+    unsolved_cells.first.solution = remainder
     true
   end
 end
