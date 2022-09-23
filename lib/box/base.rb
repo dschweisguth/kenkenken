11ee27fd0e60d6fa30c62ef9f8e140dcd8730676
Dave Schweisguth <dave@schweisguth.org>
2022-09-23 06:17:52 -0700

    Test that locations are on grid

--- lib/box/base.rb
+++ lib/box/base.rb
@@ -9,8 +9,18 @@ class Box::Base
   def initialize(grid_size, result, locations)
     @grid_size = grid_size
     @result = result
+    initialize_cells(locations)
+  end
+
+  private def initialize_cells(locations)
     @cells = {}
     locations.each do |location|
+      if location[0] < 0 || grid_size <= location[0]
+        raise "Location #{location.inspect} x is off the grid"
+      end
+      if location[1] < 0 || grid_size <= location[1]
+        raise "Location #{location.inspect} y is off the grid"
+      end
       if @cells[location]
         raise "Two cells are at the same location, #{location.inspect}"
       end
--- spec/box/base_spec.rb
+++ spec/box/base_spec.rb
@@ -2,6 +2,22 @@ require_relative '../../lib/box/base'
 
 RSpec.describe Box::Base do
   describe '.new' do
+    it "rejects x < 0" do
+      expect { Box::Base.new 1, 1, [[-1, 0]] }.to raise_error "Location [-1, 0] x is off the grid"
+    end
+
+    it "rejects x >= grid_size" do
+      expect { Box::Base.new 1, 1, [[1, 0]] }.to raise_error "Location [1, 0] x is off the grid"
+    end
+
+    it "rejects y < 0" do
+      expect { Box::Base.new 1, 1, [[0, -1]] }.to raise_error "Location [0, -1] y is off the grid"
+    end
+
+    it "rejects y >= grid_size" do
+      expect { Box::Base.new 1, 1, [[0, 1]] }.to raise_error "Location [0, 1] y is off the grid"
+    end
+
     it "rejects duplicate cells" do
       expect { Box::Base.new 2, 1, [[0, 0], [0, 0]] }.to raise_error "Two cells are at the same location, [0, 0]"
     end
